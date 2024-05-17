5174e588352dab4f41d98e78a0324785d9825af5
Abdelrhman Mamdouh <abdelrhman99m@gmail.com>
2024-05-17 23:45:33 +0300

    add new weather guide project

---
+++ Weather Guide/Weather Guide/Weather_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_GuideApp.swift
+//  Weather Guide
+//
+//  Created by Abdelrhman Mamdouh on 17/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Weather_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
